494a4c836843732311efd484633a6bb1165ca781
althafvly <althafvly@gmail.com>
2024-02-11 23:31:09 +0530

    Update gradle deps
    PlainTooltipBox Deprecated

--- app/src/main/kotlin/cz/lastaapps/languagetool/ui/components/IconButtonTooltip.kt
+++ app/src/main/kotlin/cz/lastaapps/languagetool/ui/components/IconButtonTooltip.kt
@@ -1,13 +1,30 @@
 package cz.lastaapps.languagetool.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.PlainTooltipBox
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -15,15 +32,41 @@ fun IconButtonTooltip(
     onClick: () -> Unit,
     icon: ImageVector,
     contentDescription: String,
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
 ) {
-    PlainTooltipBox(
-        tooltip = { Text(contentDescription) },
-        modifier = modifier,
+    var isTooltipVisible by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = modifier.pointerInput(Unit) {
+            detectTapGestures(
+                onPress = { isTooltipVisible = true }
+            )
+        }
     ) {
-        IconButton(onClick = onClick,
-            modifier = Modifier.tooltipAnchor()) {
+        IconButton(
+            onClick = onClick,
+            modifier = Modifier
+                .then(Modifier.clipToBounds())
+        ) {
             Icon(icon, contentDescription = contentDescription)
+        }
+
+        if (isTooltipVisible) {
+            Box(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .background(Color.Black, shape = RoundedCornerShape(4.dp))
+                    .padding(horizontal = 8.dp, vertical = 4.dp)
+                    .wrapContentWidth(Alignment.CenterHorizontally)
+                    .offset(y = (-24).dp)
+                    .alpha(0.9f)
+            ) {
+                Text(
+                    text = contentDescription,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.White
+                )
+            }
         }
     }
 }
