0ff242763f455bc6265f3e3584c9715df190905a
althafvly <althafvly@gmail.com>
2024-11-17 20:29:43 +0530

    Fix suggestions row padding

--- app/src/main/kotlin/cz/lastaapps/languagetool/ui/features/home/components/ErrorSuggestionRow.kt
+++ app/src/main/kotlin/cz/lastaapps/languagetool/ui/features/home/components/ErrorSuggestionRow.kt
@@ -1,7 +1,6 @@
 package cz.lastaapps.languagetool.ui.features.home.components
 
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -22,8 +21,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -33,7 +31,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextDecoration
@@ -49,7 +46,6 @@ import cz.lastaapps.languagetool.domain.model.MatchedText
 import cz.lastaapps.languagetool.ui.theme.PaddingTokens
 import cz.lastaapps.languagetool.ui.util.PreviewWrapper
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 internal fun ErrorSuggestionRow(
     progress: CheckProgress,
@@ -87,13 +83,13 @@ internal fun ErrorSuggestionRow(
                 onSuggestion = { onApplySuggestion(error, it) },
                 onSkip = { onSkip(error) },
                 error = error,
-                modifier = Modifier.animateItemPlacement(),
+                modifier = Modifier.animateItem(fadeInSpec = null, fadeOutSpec = null),
             )
         }
     }
 }
 
-@OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 internal fun ErrorSuggestionRowItem(
     onDetail: () -> Unit,
@@ -102,72 +98,77 @@ internal fun ErrorSuggestionRowItem(
     error: MatchedError,
     modifier: Modifier = Modifier,
 ) {
-    ElevatedCard(
-        onClick = onDetail,
-        modifier = modifier
-            .sizeIn(
-                minWidth = ErrorSuggestionTokens.minWidth,
-                maxWidth = ErrorSuggestionTokens.maxWidth - 40.dp,
-            )
-            .height(ErrorSuggestionTokens.height)
-            .padding(bottom = PaddingTokens.Small),
+    Surface(
+        color = MaterialTheme.colorScheme.surfaceVariant,
+        shape = MaterialTheme.shapes.small,
+        modifier = Modifier.padding(PaddingTokens.Tiny),
+        shadowElevation = 4.dp,
     ) {
-        Column(
+        Card(
+            onClick = onDetail,
             modifier = modifier
-                .padding(PaddingTokens.Small)
-                .clip(MaterialTheme.shapes.medium)
-                .width(IntrinsicSize.Max)
-                .verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.Start,
-            verticalArrangement = Arrangement.spacedBy(PaddingTokens.Small),
+                .sizeIn(
+                    minWidth = ErrorSuggestionTokens.minWidth,
+                    maxWidth = ErrorSuggestionTokens.maxWidth,
+                )
+                .height(ErrorSuggestionTokens.height)
         ) {
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(PaddingTokens.MidSmall),
-                verticalAlignment = Alignment.CenterVertically,
+            Column(
+                modifier = modifier
+                    .padding(PaddingTokens.Medium)
+                    .width(IntrinsicSize.Max)
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.spacedBy(PaddingTokens.Small),
             ) {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(PaddingTokens.MidSmall),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = error.shortMessage ?: error.message,
+                        softWrap = false,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.titleSmall,
+                        modifier = Modifier.weight(1f),
+                    )
+
+                    if (error.isPremium) {
+                        Icon(
+                            Icons.Default.Star,
+                            contentDescription = stringResource(id = R.string.label_premium_error),
+                            tint = Color(0xFFFBC02D),
+                        )
+                    }
+                }
+
                 Text(
-                    text = error.shortMessage ?: error.message,
+                    text = error.original,
                     softWrap = false,
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis,
-                    style = MaterialTheme.typography.titleSmall,
-                    modifier = Modifier.weight(1f),
+                    textDecoration = TextDecoration.LineThrough,
+                    style = MaterialTheme.typography.labelSmall,
                 )
 
-                if (error.isPremium) {
-                    Icon(
-                        Icons.Default.Star,
-                        contentDescription = stringResource(id = R.string.label_premium_error),
-                        tint = Color(0xFFFBC02D),
-                    )
-                }
-            }
+                FlowRow {
+                    val itemModifier = Modifier.padding(PaddingTokens.Small / 2)
 
-            Text(
-                text = error.original,
-                softWrap = false,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                textDecoration = TextDecoration.LineThrough,
-                style = MaterialTheme.typography.labelSmall,
-            )
+                    error.replacements.forEach { suggestion ->
+                        SuggestionItem(
+                            onClick = { onSuggestion(suggestion) },
+                            suggestion = suggestion,
+                            type = error.errorType,
+                            modifier = itemModifier,
+                        )
+                    }
 
-            FlowRow {
-                val itemModifier = Modifier.padding(PaddingTokens.Small / 2)
-
-                error.replacements.forEach { suggestion ->
-                    SuggestionItem(
-                        onClick = { onSuggestion(suggestion) },
-                        suggestion = suggestion,
-                        type = error.errorType,
+                    SkipItem(
+                        onClick = onSkip,
                         modifier = itemModifier,
                     )
                 }
-
-                SkipItem(
-                    onClick = onSkip,
-                    modifier = itemModifier,
-                )
             }
         }
     }
@@ -186,7 +187,7 @@ private fun SuggestionItem(
         color = color,
         modifier = modifier.clickable { onClick() },
         shape = MaterialTheme.shapes.small,
-        shadowElevation = 4.dp,
+        shadowElevation = 0.dp,
     ) {
         Text(
             text = suggestion,
@@ -207,10 +208,10 @@ private fun SkipItem(
     modifier: Modifier = Modifier,
 ) {
     Surface(
-        color = MaterialTheme.colorScheme.surfaceVariant,
+        color = MaterialTheme.colorScheme.inverseOnSurface,
         modifier = modifier.clickable { onClick() },
         shape = MaterialTheme.shapes.small,
-        shadowElevation = 4.dp,
+        shadowElevation = 0.dp,
     ) {
         Text(
             text = stringResource(id = R.string.label_skip),
@@ -228,7 +229,7 @@ private fun SkipItem(
 private object ErrorSuggestionTokens {
     val minWidth = 128.dp
     val maxWidth = 192.dp
-    val height = 128.dp
+    val height = 160.dp
     val minSuggestionHeight = 24.dp
 }
 
